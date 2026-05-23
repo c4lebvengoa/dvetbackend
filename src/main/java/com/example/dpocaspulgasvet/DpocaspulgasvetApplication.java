9fb3b11418a6b69075f37d129a7065e1fcd5d9f2
KalelUWU <calebjuan44@gmail.com>
2026-05-22 20:45:06 -0500

    subiendo archivos bua

---
+++ src/main/java/com/example/dpocaspulgasvet/DpocaspulgasvetApplication.java
@@ -0,0 +1,13 @@
+package com.example.dpocaspulgasvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpocaspulgasvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpocaspulgasvetApplication.class, args);
+	}
+
+}
